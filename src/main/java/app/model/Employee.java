15c9e4d7a40d652e92ce54a5fac8bc5ec62f34b9
nick <nicolasfarhat7@gmail.com>
2022-06-21 18:29:24 -0300

    hibernate setup correction and class modelling

--- src/main/java/app/model/Employee.java
+++ src/main/java/app/model/Employee.java
@@ -1,4 +1,4 @@
-package model;
+package app.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- src/main/java/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository{
-    
-}
