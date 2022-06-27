90429b421fde0c0e07552c4841af8d392162213c
nick <nicolasfarhat7@gmail.com>
2022-06-27 16:52:36 -0300

    adding react-app

---
+++ react-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <ListEmployeeComponent></ListEmployeeComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Employee Management System
+
+### Java, ReactJs and SpringBoot
