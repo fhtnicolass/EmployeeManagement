ef2f722551413f1d940aa4d571360ce06bc8250e
nick <nicolasfarhat7@gmail.com>
2022-07-10 17:24:24 -0300

    adding header and footer components + routes

--- employee_crud_project/react-frontend/src/App.js
+++ employee_crud_project/react-frontend/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import ListEmployeeComponent from './components/ListEmployeeComponent';
-import React from 'react';
+import "./App.css";
+import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    <div className="App">
-      <ListEmployeeComponent></ListEmployeeComponent>
+    <div>
+      <Router>
+          <HeaderComponent></HeaderComponent>
+            <div className="container">
+              <Routes>
+                <Route path = "/" element = {<ListEmployeeComponent/>}></Route>
+                <Route path = "/employees" element = {<ListEmployeeComponent/>}></Route>
+              </Routes>
+            </div>
+          <FooterComponent></FooterComponent>
+      </Router>
     </div>
   );
 }
